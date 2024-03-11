9ddc31703860d096df9dfd01319b8ee01d24d5ef
kiana-git <redu37genanaw@gmail.com>
2024-03-11 22:44:33 +0300

    commit name

--- components/connection.php
+++ components/connection.php
@@ -1,7 +1,7 @@
 <?php 
 $db_name = 'mysql:host=localhost;dbname=shop_db' ;
 $db_user = 'root';
-$db_password = '';
+$db_password ='';
 
 $conn = new PDO($db_name,$db_user,$db_password);
 
@@ -12,6 +12,9 @@ function unique_id(){
     $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' ;
     $charLength = strlen($chars);
     $randomString = '';
-    for($i=0; $i<20; )
+    for($i=0; $i<20; $i++){
+        $randomString.=$chars[mt_rand(0, $charLength - 1)];
+    }
+    return $randomString;   
 }
 ?>
